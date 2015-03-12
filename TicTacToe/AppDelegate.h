bae1dc551cd98e599760a06bd8cfe5ec45690bc6
cinnabarq <cinnabarq@gmail.com>
2015-03-12 13:19:04 -0700

    Initial Commit

---
+++ TicTacToe/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TicTacToe
+//
+//  Created by Sha Zhu on 3/12/15.
+//  Copyright (c) 2015 Cena Entertaiment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TicTacToe/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TicTacToe
+//
+//  Created by Sha Zhu on 3/12/15.
+//  Copyright (c) 2015 Cena Entertaiment. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
